83510a3348a6c498e57a23ffe8119788e37b102f
Newcayen Sevezile Maluleke <157349577+MalulekeNS@users.noreply.github.com>
2025-10-21 03:09:00 +0200

    Add code

---
+++ EduPathApplication.java
@@ -0,0 +1,11 @@
+package com.edupath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPathApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduPathApplication.class, args);
+    }
+}
